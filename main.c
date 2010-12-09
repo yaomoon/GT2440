e55a8f3b79a00af224b1102b3a51ad2e502bee29
moon <moon485@163.com>
2010-12-09 03:08:17 -0800

    wo de

---
+++ main.c
@@ -0,0 +1,26 @@
+#include"hello.h" 
+#define how 3
+#include <stdio.h>
+lkinlkkgiaf
+lkinlkkgiaf
+lkinlkkgiaf
+
+lkinlkkgiaf
+nl
+int main(int argc, const char *argv[])
+{
+when have
+
+when have
+
+when have
+
+when have
+
+   int i; 
+    for (i = 0; i < how; i++) 
+    {
+        say_hello();
+    }
+    return 0;
+}
