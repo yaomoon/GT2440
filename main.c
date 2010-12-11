16dc04497a3fca47e124ea238496d07414a9d6b5
moon <moon485@163.com>
2010-12-11 00:04:20 -0800

    yaomingyuemoon

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include"hello.h" 
 #define how 3
 #include <stdio.h>
-lkinlkkgiaf
-lkinlkkgiaf
-lkinlkkgiaf
-
-lkinlkkgiaf
-nl
 int main(int argc, const char *argv[])
 {
 
-when have
 
-when have
 
    int i; 
     for (i = 0; i < how; i++) 
